a6250df2cd722addebb35a0001bde6a8729d170e
nightness <joshguyette@yahoo.com>
2021-06-04 16:54:52 -0500

    Prevent unsetting of the display name

--- functions/src/users/setProfileAttribute.ts
+++ functions/src/users/setProfileAttribute.ts
@@ -6,15 +6,19 @@ import { UserProfile } from '../shared'
 /// setProfileAttribute
 ///
 export default functions.https.onCall(async (data, context) => {
-    if (data === undefined || data.authToken === undefined || data.displayName === undefined)
-        return ({ result: 'Invalid arguments passed' })
+    if (
+        data === undefined ||
+        data.authToken === undefined ||
+        data.displayName === undefined
+    )
+        return { result: 'Invalid arguments passed' }
 
     const userInfo = await admin.auth().verifyIdToken(data.authToken)
-    if (!userInfo)
-        return ({ result: 'Invalid Authentication' })
+    if (!userInfo) return { result: 'Invalid Authentication' }
 
     const doc: UserProfile = {}
-    doc.displayName = data.displayName
+    if (typeof data.displayName === 'string' && data.displayName !== '')
+        doc.displayName = data.displayName
     doc.theme = data?.theme
     doc.photoURL = userInfo?.picture
     return await admin.firestore().collection('profiles').doc(userInfo.uid).set(doc)
