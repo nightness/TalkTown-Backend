a785adb0b31467922c376ff745203b6c614facec
nightness <joshguyette@yahoo.com>
2021-06-05 20:55:12 -0500

    write notification messages to each member of a group

--- functions/src/messenger/setMessage.ts
+++ functions/src/messenger/setMessage.ts
@@ -5,7 +5,6 @@ import { getGroupMembers, setNotifications } from '../messenger/shared'
 
 const emoji = require('emoji-node')
 const Filter = require('bad-words')
-const getUserProfile = require('../shared/getUserProfile').getUserProfile
 
 ///
 /// setMessage
@@ -15,51 +14,50 @@ export default functions.https.onCall(async (data, context) => {
     // !!! Enforce security rules locally!!!
     // !!! Firestore rules do not apply in here so this function must makes those checks!!!
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    const currentUserUid = context.auth ? context.auth.uid : null;
+    const currentUserUid = context.auth ? context.auth.uid : null
     if (!currentUserUid)
         return errorGenerator.error('You must be authenticated to use this function')
 
-    if (!data.collectionPath)
-        return errorGenerator.error('collectionPath is required')
+    if (!data.collectionPath) return errorGenerator.error('collectionPath is required')
 
     // Prevent zero length messages and non-string messages
     var message = data.message
-    if (!message || typeof (message) !== 'string' || message.length < 1)
-        return errorGenerator.silentError('Message length equals zero or message is not a string')
+    if (!message || typeof message !== 'string' || message.length < 1)
+        return errorGenerator.silentError(
+            'Message length equals zero or message is not a string'
+        )
 
     // Stop bad language posts!
     const filter = new Filter()
     if (filter.isProfane(message)) {
-        return errorGenerator.silentError(`Remove the profanity in your message before posting!`)
+        return errorGenerator.silentError(
+            `Remove the profanity in your message before posting!`
+        )
     }
 
     // Process message with emoji, do a Emoticon to Emoji conversion. ;( => 😢
     message = emoji.emojify(message)
 
-    // Get the sender's user profile, to get their name
-    let senderProfile = await (async () => {
-        try {
-            return await getUserProfile(currentUserUid)
-        } catch (error) {
-            return errorGenerator.error(error.message)
-        }
-    })()
+    const userInfo = await admin.auth().getUser(currentUserUid)
+    if (typeof userInfo.displayName !== 'string')
+        return errorGenerator.silentError(
+            `No display name is specified in the user's info`
+        )
 
     // Private Messages
-    if (!data.documentId && senderProfile?.displayName) {
+    if (!data.documentId) {
         const messagesCollection = admin.firestore().collection(data.collectionPath)
         const addData = {
-            authorName: senderProfile.displayName,
+            authorName: userInfo.displayName,
             authorUid: currentUserUid,
             message: message,
             postedAt: admin.firestore.FieldValue.serverTimestamp(),
-            photoURL: senderProfile?.photoURL
+            photoURL: userInfo?.photoURL,
         }
 
         const writeResult1 = await messagesCollection.add(addData)
 
-        if (!data.duplicationPath)
-            return writeResult1
+        if (!data.duplicationPath) return writeResult1
 
         const duplicateCollection = admin.firestore().collection(data.duplicationPath)
         const writeResult2 = await duplicateCollection.add(addData)
@@ -68,19 +66,25 @@ export default functions.https.onCall(async (data, context) => {
     }
 
     // Public Messages
-    const memberDoc = admin.firestore().collection(data.collectionPath).doc(data.documentId)
+    const memberDoc = admin
+        .firestore()
+        .collection(data.collectionPath)
+        .doc(data.documentId)
     const messagesCollection = memberDoc.collection('messages')
     const addData = {
-        authorName: senderProfile.displayName,
+        authorName: userInfo.displayName,
         authorUid: currentUserUid,
         message: message,
         postedAt: admin.firestore.FieldValue.serverTimestamp(),
-        photoURL: senderProfile?.photoURL
+        photoURL: userInfo?.photoURL,
     }
 
     const writeResult1 = await messagesCollection.add(addData)
 
-    const userIds = (await getGroupMembers(data.documentId))
+    const userIds = await getGroupMembers(data.documentId)
+    if (!userIds || !Array.isArray(userIds) || userIds.length === 0)
+        return { result1: writeResult1 }
+
     const writeResult2 = await setNotifications(userIds, data.documentId)
 
     // setNotifications is always returning null, since it using a loop though userIds
--- functions/src/messenger/shared.ts
+++ functions/src/messenger/shared.ts
@@ -4,40 +4,44 @@ import * as admin from 'firebase-admin'
 /// getGroupMembers
 ///
 export const getGroupMembers = async (groupId: string) => {
-    if (typeof groupId !== 'string')
-        throw new Error('Bad Call')
+    if (typeof groupId !== 'string') throw new Error('Bad Call')
 
     const docRef = admin.firestore().collection('groups').doc(groupId)
     const doc = await docRef.get()
-    const data = doc.exists ? doc.data() : []
-    return data as string[]
+    const data = doc.exists ? doc.data() : undefined
+    return (data as FirebaseFirestore.DocumentData).members as string[]
 }
 
 ///
 /// setNotifications
 ///
-export const setNotifications = async (userIds: string[], groupId: string, reset = false, remove = false) => {
+export const setNotifications = async (
+    userIds: string[],
+    groupId: string,
+    reset = false,
+    remove = false
+) => {
     const collectionRef = admin.firestore().collection('notifications')
-    userIds.forEach(async (userId) => {
+    userIds.map(async (userId) => {
         const docRef = collectionRef.doc(userId)
         const doc = await docRef.get()
-        const data = doc.exists ? doc.data() : { groups: { } }
-        const notifications = data ? data : { groups: { } }
-    
-        if (!notifications.groups)
-            notifications.groups = { }
-    
+        const data = doc.exists ? doc.data() : { groups: {} }
+        const notifications = data ? data : { groups: {} }
+
+        if (!notifications.groups) notifications.groups = {}
+
         // Make changes
         if (remove && notifications.groups[groupId] !== undefined) {
             delete notifications.groups[groupId]
         } else {
             // reset or increase the count of unseen messages for the specified group
-            notifications.groups[groupId] = reset ? 0 : 
-                notifications.groups[groupId] ? notifications.groups[groupId] + 1 : 1
+            notifications.groups[groupId] = reset
+                ? 0
+                : notifications.groups[groupId]
+                ? notifications.groups[groupId] + 1
+                : 1
         }
         // Write the notifications doc
         await docRef.set(notifications)
     })
-    
 }
-
