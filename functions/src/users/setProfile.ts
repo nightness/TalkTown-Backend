9ce6eddd93f3c0417d72e561421b2965dc37eb82
nightness <joshguyette@yahoo.com>
2021-06-06 19:27:08 -0500

    renamed setProfileAttrribute to setProfile and cleaned it up a bit

--- functions/src/users/setProfile.ts
+++ functions/src/users/setProfile.ts
@@ -18,7 +18,12 @@ export default functions.https.onCall(async (data, context) => {
     if (userInfo.displayName) doc.displayName = userInfo.displayName
     // Only allow displayName to be set if it's not already set.
     if (!doc.displayName && data.displayName) doc.displayName = data.displayName
+    // Change theme
     if (data.theme) doc.theme = data.theme
-    if (userInfo.photoURL) doc.photoURL = userInfo.photoURL
+    // Use the data.photoURL first
+    if (data.photoURL) doc.photoURL = data.photoURL
+    // If not set, keep the previous photoURL
+    if (!doc.photoURL && userInfo.photoURL) doc.photoURL = userInfo.photoURL
+    // Set profile document
     return await admin.firestore().collection('profiles').doc(userInfo.uid).set(doc)
 })
