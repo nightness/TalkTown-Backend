c02cfd3b25c8f22982020bdebb11d9f3598387ad
Josh Guyette <joshguyette@yahoo.com>
2022-05-03 19:53:40 -0500

    final commit before renaming project

--- functions/src/webrtc/createCall.ts
+++ functions/src/webrtc/createCall.ts
@@ -5,7 +5,7 @@ export default functions.https.onCall(async ({ target }, context) => {
     if (!context.auth) return { error: 'Not Authenticated' }
 
     const result = await admin.auth().getUser(context.auth.uid)
-    if (!result.emailVerified) return { error: 'Permission Denied' }
+    if (!result.emailVerified) return { error: 'Permission Denied: Verify your email address' }
 
     if (typeof target !== 'string') return { error: 'Invalid arguments passed' }
 
